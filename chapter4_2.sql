122ee5131cb4f39fe3bde20803b33f3f4bc0e93c
zihua <1477765176@qq.com>
2016-12-11 00:22:29 +0800

    实验二

--- chapter4_2.sql
+++ chapter4_2.sql
@@ -38,3 +38,19 @@ select * from chapter4_2 order by gdp;   //默认是  以gdp 升序排序
 select * from chapter4_2 order by gdp asc;  //也是升序
 
 select * from chapter4_2 order by gdp desc;//下降排序  
+
+
+
+//数据库试验 2
+//第一问
+select student_id,name from student where brithday-2011>=20;
+
+//第二问
+select crouse_id from select_table where student_id!=(select student_id from student where name='刘晓明');
+
+//第三问
+select season ,crouse_id,crouse_time from open_crouse where work_id=(select work_id from teacher where name='马小红');
+//第四问
+
+select crouse_id,crouse_name,from crouse where crouse_id  in (select crouse_id, work_id from select_table where score_f>=60 and student_id in (select student_id from student where sex='男'));
+
